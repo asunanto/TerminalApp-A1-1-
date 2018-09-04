b5d5565edc9353015c933ad10830656e59b15484
asunanto <wibowo.sutanto91@gmail.com>
2018-09-04 14:10:34 +1000

    add feature implemented in class

--- src/WeekendDecider.rb
+++ src/WeekendDecider.rb
@@ -3,8 +3,13 @@ class WeekendList
     def initialize(list)
         @list = list
     end
+    def add(hash)
+        @list << hash
+    end
 end
 
 list_of_actities = [{activity: "basketball", time: 1},{activity: "tennis", time:2}]
 weekend = WeekendList.new(list_of_actities)
+hash = {activity: "soccer", time:1}
+weekend.add(hash)
 puts weekend.list
