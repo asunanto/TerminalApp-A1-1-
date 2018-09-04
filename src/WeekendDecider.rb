29ed630beffcc9dddb6215080f7d8da3674d4efc
asunanto <wibowo.sutanto91@gmail.com>
2018-09-04 14:47:38 +1000

    added a delete function to class

--- src/WeekendDecider.rb
+++ src/WeekendDecider.rb
@@ -6,6 +6,9 @@ class WeekendList
     def add(hash)
         @list << hash
     end
+    def delete(activity)
+        @list.delete_if { |hash| hash[:activity] == activity }
+    end
 end
 
 list_of_actities = [{activity: "basketball", time: 1},{activity: "tennis", time:2}]
@@ -13,3 +16,7 @@ weekend = WeekendList.new(list_of_actities)
 hash = {activity: "soccer", time:1}
 weekend.add(hash)
 puts weekend.list
+puts
+weekend.delete("tennis")
+puts weekend.list
+
